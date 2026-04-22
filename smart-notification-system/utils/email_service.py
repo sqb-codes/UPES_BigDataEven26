18494529f8518aa6b51728f330aeb784ef94574a
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2026-04-22 09:38:05 +0530

    Add files via upload

---
+++ smart-notification-system/utils/email_service.py
@@ -0,0 +1,31 @@
+# SMTP library to send mails
+import smtplib
+# to format the email content
+from email.mime.text import MIMEText
+
+def send_mail(subject:str, body:str):
+    """
+    Sends an email using Gmail SMTP server
+    :param subject: email subject
+    :param body: body of the mail
+    :return: none
+    """
+    sender = "sender@gmail.com"
+    password = "16 digit app password"
+
+    receiver = "receiver@gmail.com"
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            # Start TLS encryption for secure connection
+            server.login(sender, password)
+            server.sendmail(sender, receiver, msg.as_string())
+        print("Email Sent Successfully...")
+    except Exception as ex:
+        print("Failed to send mail:",ex)
